e12b647979a43cdba6b4ff608eeec3103fe14050
David Buschtöns <uccug@student.kit.edu>
2017-03-05 16:42:34 +0100

    Add Kunyomi to KanjiPage

--- src/KanjiPage.ts
+++ src/KanjiPage.ts
@@ -1,9 +1,10 @@
-import { Page, TextView, ScrollView, ImageView, Composite } from 'tabris';
+import { Page, TextView, ScrollView, ImageView, Composite, Widget } from 'tabris';
 import { IDictionaryEntry, IJukugo, IKunyomi, ILookalikeSet } from './interfaces';
 import { parseImage, getUsefulnessStars, createKanji, getOnyomi } from './util';
 import { applyColors } from './resources';
 
 const MAIN_KANJI_SIZE = 80;
+const TEMP_KUN_KANJI_SETTING = true;
 
 export default class KanjiPage extends Page {
 
@@ -16,17 +17,20 @@ export default class KanjiPage extends Page {
     scrollView.append(
       createKanji(data, MAIN_KANJI_SIZE),
       this.createComponentsDisplay(data.components),
-      new TextView({ class: 'usefulness', text: getUsefulnessStars(data), font: '20px' }),
+      new TextView({ class: 'usefulness', id: 'usefulness', text: getUsefulnessStars(data), font: '20px' }),
       new TextView({ class: 'strokeCount', text: data.strokeCount + ' strokes', font: '15px' }),
       new TextView({ class: 'translation', text: data.meaning, font: '30px' }),
       new TextView({ class: 'number', text: 'number ' + data.number, font: '16px' }),
       new TextView({ class: 'label', id: 'onLabel', text: 'On: ', font: '24px' }),
       new TextView({ class: 'onyomi', text: getOnyomi(data), font: '24px' }),
-      new TextView({ class: 'mnemonic', text: data.mnemonic, font: "20px", markupEnabled: true })
+      new TextView({ class: 'mnemonic', text: data.mnemonic, font: "18px", markupEnabled: true })
     )
-    data.kunyomi.forEach(kunyomi => {
-      this.createKunyomiDisplay(kunyomi).appendTo(scrollView);
-    });
+    if (data.kunyomi && data.kunyomi.length > 0) {
+      new TextView({ class: 'label', id: 'kunLabel', text: 'Kunyomi: ', font: '26px' }).appendTo(scrollView),
+        data.kunyomi.forEach(kunyomi => {
+          this.createKunyomiDisplay(kunyomi).appendTo(scrollView);
+        });
+    }
     data.jukugo.forEach(jukugo => {
       this.createJukugoDisplay(jukugo).appendTo(scrollView);
     });
@@ -40,19 +44,38 @@ export default class KanjiPage extends Page {
   }
 
   createComponentsDisplay(components: { kanji: string, kanjiImageSource: string, meaning: string }[]) {
-    let composite = new Composite({ class: 'components'});
+    let composite = new Composite({ class: 'components' });
     let prev: any = 0;
     components.forEach(component => {
-      prev = new TextView({ top: 0, left: prev, class: 'componentKanji', text: component.kanji + ' '}).appendTo(composite);
+      prev = new TextView({ top: 0, left: prev, class: 'componentKanji', text: component.kanji + ' ' }).appendTo(composite);
       let meaning = '(' + component.meaning + ')';
       meaning += components.indexOf(component) == components.length - 1 ? '' : ' + '
-      prev = new TextView({ top: 0, left: prev, class: 'componentMeaning', text: meaning}).appendTo(composite);
+      prev = new TextView({ top: 0, left: prev, class: 'componentMeaning', text: meaning }).appendTo(composite);
     });
     return composite;
   }
 
   createKunyomiDisplay(kunyomi: IKunyomi) {
-    return new Composite();
+    let composite = new Composite({ class: 'kunyomi' });
+    let offset = 5;
+    let stars = new TextView({ class: 'usefulness', left: 0, top: 0, text: getUsefulnessStars(kunyomi) }).appendTo(composite);
+    let prev = new TextView({ class: 'kunMeaning', left: stars, top: [stars, 3], right: 0, text: kunyomi.translation }).appendTo(composite);
+    if (kunyomi.postParticle != null) {
+      prev = new TextView({ class: 'post particle', right: [prev, offset], top: stars, text: kunyomi.postParticle }).appendTo(composite);
+      offset = 0;
+    }
+    if (kunyomi.okurigana != null) {
+      prev = new TextView({ class: 'kun okurigana', right: [prev, offset], top: stars, text: kunyomi.okurigana }).appendTo(composite);
+      offset = 0;
+      if (TEMP_KUN_KANJI_SETTING) prev = new TextView({ class: 'kun okuriStar', right: prev, top: stars, text: '*' }).appendTo(composite);
+    }
+    prev = new TextView({ class: 'kun reading', right: [prev, offset], top: stars, text: kunyomi.reading }).appendTo(composite);
+    if (kunyomi.preParticle != null) prev = new TextView({ class: 'pre particle', right: prev, top: stars, text: kunyomi.preParticle }).appendTo(composite);
+    composite.apply({
+      '*': { font: '18px' },
+      '.kunMeaning': { font: '16px' }
+    });
+    return composite;
   }
 
   createJukugoDisplay(jukugo: IJukugo) {
@@ -70,14 +93,16 @@ export default class KanjiPage extends Page {
   applyLayout() {
     this.apply({
       '.number': { centerX: 0, top: 5 },
-      '.usefulness': { right: 10, top: 10 },
+      '#usefulness': { right: 10, top: 10 },
       '.strokeCount': { right: 10, top: ['.usefulness', 2] },
       '.kanji': { left: 20, top: 20 },
-      '.components' : { left: 20, top: ['.kanji', 0]},
+      '.components': { left: 20, top: ['.kanji', 0] },
       '.translation': { left: ['.kanji', 16], top: 35 },
       '#onLabel': { left: ['.kanji', 16], top: ['.translation', 0] },
       '.onyomi': { left: ['#onLabel', 0], baseline: '#onLabel' },
-      '.mnemonic': { top: ['.components', 5], left: 20, right: 20 }
+      '.mnemonic': { top: ['.components', 5], left: 20, right: 20 },
+      '#kunLabel': { left: 10, top: ['.mnemonic', 5] },
+      '.kunyomi': { top: ['prev()', 0], left: 20, right: 20 }
     });
   }
 
